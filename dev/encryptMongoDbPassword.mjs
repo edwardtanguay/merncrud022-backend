d967c0dc276e82b4a94daedb76e0986003393934
Edward Tanguay (windows) <edward@tanguay.info>
2023-01-24 17:38:40 +0100

    finished

--- dev/encryptMongoDbPassword.mjs
+++ dev/encryptMongoDbPassword.mjs
@@ -2,5 +2,5 @@ import Cryptr from 'cryptr';
 
 const cryptr = new Cryptr('SECRETKEY'); // set as same in config.ts
 
-const encryptedPassword = cryptr.encrypt('PASSWORD'); // insert password, run script, DELETE PASSWORD
+const encryptedPassword = cryptr.encrypt('PASSWORD'); // insert password, run script, copy hash into config.ts, DELETE PASSWORD
 console.log(encryptedPassword);
